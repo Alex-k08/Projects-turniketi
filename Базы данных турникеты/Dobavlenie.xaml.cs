cd1a17134ed6ff6b61db84604d2c2a4d95c30eb8
unknown <you@example.com>
2022-09-18 19:19:47 +0800

    first commit

---
+++ Базы данных турникеты/Dobavlenie.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Базы_данных_турникеты
+{
+    /// <summary>
+    /// Логика взаимодействия для Dobavlenie.xaml
+    /// </summary>
+    public partial class Dobavlenie : Page
+    {
+        DataGrid dataTable;
+        Gimnazia1Entities3 db;
+        public Dobavlenie(DataGrid table, Gimnazia1Entities3 db)
+        {
+            InitializeComponent();
+            dataTable = table;
+            this.db = db;
+        }
+
+        private void cancel_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+
+        }
+
+        private void save_Click(object sender, RoutedEventArgs e)
+        {
+            this.Cursor = Cursors.Wait;
+
+            Events codeStruct = new Events
+            {
+                Contents = codeRegionEn.Text,
+              
+            };  // инициализируем объект codes и складываем в него значения из TextBox
+
+            db.Events.Add(codeStruct); // добавляем его в локальную структуру Entity
+            db.SaveChanges(); // Сохраняем изменения
+            this.Cursor = Cursors.Arrow; // Переводим курсор в обычное состояние
+            dataTable.ItemsSource = null; // Обнуляем список в DataGrid
+            dataTable.ItemsSource = db.Events.ToList(); // Запрашиваем и присваиваем новые значения
+            NavigationService.GoBack();
+        }
+
+    }
+}
---
+++ Базы данных турникеты/Page1.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Базы_данных_турникеты
+{
+    /// <summary>
+    /// Логика взаимодействия для Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        DataGrid dataTable;
+        Gimnazia1Entities3 db;
+        public Page1()
+        {
+            InitializeComponent();
+            Gimnazia1Entities3 db = new Gimnazia1Entities3();
+            data_table.ItemsSource = db.Events.ToList();
+        }
+
+        private void prohod_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Dobavlenie());
+        }
+    }
+}
